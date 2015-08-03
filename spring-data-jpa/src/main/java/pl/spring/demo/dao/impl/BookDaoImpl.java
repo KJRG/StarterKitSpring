1251799e55f7b5877b3167123a7c78956827aab8
KGALKA <krzysztof.galka@capgemini.com>
2015-08-03 10:58:05 +0200

    Removed unnecessary comments from BookDaoImpl

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
@@ -74,16 +74,6 @@ public class BookDaoImpl implements BookDao {
 	}
 
 	private void addTestBooks() {
-		// ALL_BOOKS.add(new BookTo(1L, "Romeo i Julia", "Wiliam Szekspir"));
-		// ALL_BOOKS.add(new BookTo(2L, "Opium w rosole", "Hanna Ożogowska"));
-		// ALL_BOOKS.add(new BookTo(3L, "Przygody Odyseusza", "Jan
-		// Parandowski"));
-		// ALL_BOOKS.add(new BookTo(4L, "Awantura w Niekłaju", "Edmund
-		// Niziurski"));
-		// ALL_BOOKS.add(new BookTo(5L, "Pan Samochodzik i Fantomas", "Zbigniew
-		// Nienacki"));
-		// ALL_BOOKS.add(new BookTo(6L, "Zemsta", "Aleksander Fredro"));
-
 		ALL_BOOKS.add(new BookEntity(1L, "Romeo i Julia",
 				Arrays.asList(new AuthorTo(0L, "Wiliam", "Szekspir"))));
 
