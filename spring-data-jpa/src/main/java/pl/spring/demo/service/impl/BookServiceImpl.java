4ecc6b338a8f3c3602f32fe76f73d2e6ab89e56c
KGALKA <krzysztof.galka@capgemini.com>
2015-08-04 11:07:48 +0200

    Changed annotation of BookServiceImpl to Service

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -10,9 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class BookServiceImpl implements BookService {
 
 	@Autowired
