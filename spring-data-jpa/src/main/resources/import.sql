af87f0caf308893343837568adbff71208a16c2e
KGALKA <krzysztof.galka@capgemini.com>
2015-08-07 09:05:40 +0200

    Added books

--- spring-data-jpa/src/main/resources/import.sql
+++ spring-data-jpa/src/main/resources/import.sql
@@ -1,3 +1,9 @@
 insert into book (id, title, authors) values (1, 'Pierwsza książka', 'Jan Kowalski');
 insert into book (id, title, authors) values (2, 'Druga książka', 'Zbigniew Nowak');
 insert into book (id, title, authors) values (3, 'Trzecia książka', 'Janusz Jankowski');
+insert into book (id, title, authors) values (4, 'Ostatnie życzenie', 'Andrzej Sapkowski');
+insert into book (id, title, authors) values (5, 'Kolor magii', 'Terry Pratchett');
+insert into book (id, title, authors) values (6, 'Coś', 'Stephen King');
+insert into book (id, title, authors) values (7, 'Mistrz i Małgorzata', 'Michaił Bułhakow');
+insert into book (id, title, authors) values (8, 'Drużyna pierścienia', 'John Ronald Reuel Tolkien');
+insert into book (id, title, authors) values (9, 'Krzyżacy', 'Henryk Sienkiewicz');
