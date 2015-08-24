dfd3481ae49845586ac1d896779adb43fabcc758
KGALKA <krzysztof.galka@capgemini.com>
2015-08-24 09:42:48 +0200

    Author search filter

--- web/src/main/client/app/authors/js/authors.module.js
+++ web/src/main/client/app/authors/js/authors.module.js
@@ -1,11 +1,3 @@
-// angular.module('app.authors', ['ngRoute']).config(function ($routeProvider) {
-// 	'use strict';
-// 	$routeProvider.when('/authors/author-list', {
-// 		templateUrl: 'authors/html/author-list.html',
-// 		controller: 'AuthorSearchController'
-// 	});
-// });
-
 angular.module('app.authors', ['ngRoute']).config(function ($routeProvider) {
     'use strict';
     $routeProvider.when('/authors/author-list', {
