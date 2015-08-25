597d186b41c49941f4c0f2a2167026923d7e01f1
KGALKA <krzysztof.galka@capgemini.com>
2015-08-25 16:09:42 +0200

    Book rest service tests - prototype

---
+++ web/src/main/client/app/books/js/book-search.rest.service.spec.js
@@ -0,0 +1,51 @@
+describe('book rest service', function () {
+	'use strict';
+	
+	beforeEach(function () {
+		module('app.main');
+		module('app.books');
+	});
+	
+	var scope, httpBackend, bookRestService;
+	beforeEach(inject(function ($rootScope, _bookRestService_, $httpBackend) {
+		scope = $rootScope.$new();
+		httpBackend = $httpBackend;
+		bookRestService = _bookRestService_;
+	}));
+	
+	it('rest service should call GET', inject(function ($q) {
+		// given
+		var prefix = 'test';
+		var result = {data: [{id: 1, title: 'test1'}, {id: 2, title: 'test2'}]};
+		var getDeferred = $q.defer();
+		httpBackend.when('GET', '/context.html/rest/books/books-by-title?titlePrefix=' + prefix)
+					.respond(getDeferred.promise);
+		
+		// when
+		// var response = bookRestService.search(prefix);
+		bookRestService.search(prefix);
+		httpBackend.flush();
+		getDeferred.resolve(result);
+		scope.$digest();
+		
+		// then
+		// expect(response.length).toBe(2);
+		httpBackend.expect('GET', '/context.html/rest/books/books-by-title?titlePrefix=' + prefix);
+	}));
+
+	it('rest service should call POST', function () {
+		// given
+		var book = {
+			id: 1,
+			title: 'test',
+			authors: []
+		};
+		
+		// then
+		httpBackend.expect('POST', '/context.html/rest/books/book').respond(book);
+
+		// when
+		bookRestService.saveBook(book);
+		httpBackend.flush();
+	});
+});
