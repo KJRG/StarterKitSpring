96a56f59046c59a99a681ffc8773afaeb69995a2
KGALKA <krzysztof.galka@capgemini.com>
2015-08-26 16:00:21 +0200

    Modified author service test

--- web/src/main/client/app/authors/js/author-search.service.spec.js
+++ web/src/main/client/app/authors/js/author-search.service.spec.js
@@ -1,23 +1,32 @@
 describe('author service', function () {
 	'use strict';
 	
-	var authorService, authorRestServiceMock;
+	var $scope, authorService;
 	beforeEach(function () {
 		module('app.main');
-		module('app.authors', function ($provide) {
-			authorRestServiceMock = jasmine.createSpyObj('authorRestService', ['search']);
-			$provide.value('authorRestService', authorRestServiceMock);
-		});
+		module('app.authors');
 		
-		inject(function (_authorService_) {
+		inject( function (_authorService_) {
 			authorService = _authorService_;
 		});
 	});
 	
-	it('authorService should call authorRestService', function () {
-		// given when
+	beforeEach(inject (function ($rootScope) {
+		$scope = $rootScope.$new();
+	}));
+	
+	it('search should call authorRestService.search', inject(function ($q, authorRestService) {
+		// given
+		var result = {data: [{id: 1, firstName: 'Test', lastName: 'Author One'},
+						{id: 2, firstName: 'Test', lastName: 'Author Two'},
+						{id: 3, firstName: 'Test', lastName: 'Author Three'}] };
+		var searchDeferred = $q.defer();
+		spyOn(authorRestService, 'search').and.returnValue(searchDeferred.promise);
+		// when
 		authorService.search();
+		searchDeferred.resolve(result);
+		$scope.$digest();
 		// then
-		expect(authorRestServiceMock.search).toHaveBeenCalledWith();
-	});
+		expect(authorRestService.search).toHaveBeenCalledWith();
+	}));
 });
