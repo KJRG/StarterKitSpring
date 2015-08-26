468d3820273678e2f2884b05adb5425bd6648028
KGALKA <krzysztof.galka@capgemini.com>
2015-08-26 12:04:38 +0200

    Modified book service tests

--- web/src/main/client/app/books/js/book-search.service.spec.js
+++ web/src/main/client/app/books/js/book-search.service.spec.js
@@ -1,59 +1,76 @@
 describe('book service should call book rest service', function () {
 	'use strict';
 	
-	var bookService, bookRestServiceMock;
+	var $scope, bookService;
 	beforeEach(function () {
 		module('app.main');
-		module('app.books', function ($provide) {
-			bookRestServiceMock = jasmine.createSpyObj('bookRestService',
-					['search', 'saveBook', 'editBookTitle', 'deleteBook']);
-			$provide.value('bookRestService', bookRestServiceMock);
-		});
+		module('app.books');
 		
 		inject( function (_bookService_) {
 			bookService = _bookService_;
 		});
 	});
 	
-	it('book service should call search', function () {
+	beforeEach(inject (function ($rootScope) {
+		$scope = $rootScope.$new();
+	}));
+	
+	it('search should call bookRestService.search', inject(function ($q, bookRestService) {
 		// given
 		var prefix = 'test';
+		var result = {data: [{id: 5, title: 'Test 1', authors: [{id: 7, firstName: 'Jan', lastName: 'Kowalski'}]}] };
+		var searchDeferred = $q.defer();
+		spyOn(bookRestService, 'search').and.returnValue(searchDeferred.promise);
 		// when
 		bookService.search(prefix);
-		// then
-		expect(bookRestServiceMock.search).toHaveBeenCalledWith(prefix);
-	});
+        searchDeferred.resolve(result);
+        $scope.$digest();
+        // then
+        expect(bookRestService.search).toHaveBeenCalledWith(prefix);
+	}));
 	
-	it('book service should call saveBook', function () {
+	it('saveBook should call bookRestService.searchBook', inject( function ($q, bookRestService) {
 		// given
 		var book = {
 			id: 1,
-			title: 'test'
+			authors: [{id: 1, firstName: 'Jan', lastName: 'Kowalski'}]
 		};
+		var result = {data: [{id: 1, title: 'test', authors: [{id: 1, firstName: 'Jan', lastName: 'Kowalski'}]}] };
+		var saveBookDeferred = $q.defer();
+		spyOn(bookRestService, 'saveBook').and.returnValue(saveBookDeferred.promise);
 		// when
 		bookService.saveBook(book);
+		saveBookDeferred.resolve(result);
+		$scope.$digest();
 		// then
-		expect(bookRestServiceMock.saveBook).toHaveBeenCalledWith(book);
-	});
+		expect(bookRestService.saveBook).toHaveBeenCalledWith(book);
+	}));
 	
-	it('book service should call editBookTitle', function () {
+	it('editBookTitle should call bookRestService.editBookTitle', inject( function ($q, bookRestService) {
 		// given
 		var book = {
 			id: 1,
-			title: 'test'
+			title: 'test',
+			authors: [{id: 1, firstName: 'Jan', lastName: 'Kowalski'}]
 		};
+		var result = {data: [{id: 1, title: 'test', authors: [{id: 1, firstName: 'Jan', lastName: 'Kowalski'}]}] };
+		var editBookTitleDeferred = $q.defer();
+		spyOn(bookRestService, 'editBookTitle').and.returnValue(editBookTitleDeferred.promise);
 		// when
 		bookService.editBookTitle(book);
+		editBookTitleDeferred.resolve(result);
+		$scope.$digest();
 		// then
-		expect(bookRestServiceMock.editBookTitle).toHaveBeenCalledWith(book);
-	});
+		expect(bookRestService.editBookTitle).toHaveBeenCalledWith(book);
+	}));
 	
-	it('book service should call deleteBook', function () {
+	it('deleteBook should call bookRestService.deleteBook', inject( function ($q, bookRestService) {
 		// given
 		var bookId = 1;
+		spyOn(bookRestService, 'deleteBook');
 		// when
 		bookService.deleteBook(bookId);
 		// then
-		expect(bookRestServiceMock.deleteBook).toHaveBeenCalledWith(bookId);
-	});
+		expect(bookRestService.deleteBook).toHaveBeenCalledWith(bookId);
+	}));
 });
